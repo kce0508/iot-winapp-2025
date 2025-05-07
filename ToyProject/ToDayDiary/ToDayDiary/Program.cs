955339df6e69def21997aff76cda3ef0c9e1ffa5
kce0508 <kce0508@gmail.com>
2025-05-07 09:42:59 +0900

    d

--- ToyProject/ToDayDiary/ToDayDiary/Program.cs
+++ ToyProject/ToDayDiary/ToDayDiary/Program.cs
@@ -1,4 +1,4 @@
-namespace ToyProject
+namespace ToDayDiary
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace ToyProject
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
